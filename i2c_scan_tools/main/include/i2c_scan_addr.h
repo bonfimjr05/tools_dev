f177439649e88892b9c70880f6b79bc7466efb1e
Paulo Bonfim Jr <bonfimjr.eng@gmail.com>
2020-05-29 11:39:46 -0300

    update file i2c_scan_addr

--- i2c_scan_tools/main/include/i2c_scan_addr.h
+++ i2c_scan_tools/main/include/i2c_scan_addr.h
@@ -8,7 +8,8 @@
  *@brief Scan device addresses, detects multiple devices
  */
 
-
+#ifndef I2C_SCAN_ADDR_H
+#define I2C_SCAN_ADDR_H
 /*************************************************** Includes ************************************************/
 
 #include <stdio.h>
@@ -53,3 +54,6 @@ typedef int8_t (*i2c_scan_fptr_t)(uint8_t dev_addr);
  */
 
 void i2c_scan(struct i2c_dev_t *dev);
+
+
+#endif
